6c52d299ba82198b0764a821f085dc6cb61d35de
William Carlson <wcarlson81@gmail.com>
2017-08-11 10:53:50 -0500

    Final versions

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,24 @@
-#write your code here
+def echo input
+  input
+end
+
+def shout input
+  input.upcase
+end
+
+def repeat(input, numTimes = 2)
+    ([input] * numTimes).join(" ")
+  end
+
+def start_of_word(word, numLetters)
+  word[0,numLetters]
+end
+
+def first_word word
+  word.partition(" ").first
+end
+
+def titleize word
+  stop_words = %w(and in the of a an over)
+    word.capitalize.split.map{|w| stop_words.include?(w) ? w : w.capitalize}.join(' ')
+end

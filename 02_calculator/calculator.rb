d528ad1e237a3b6e32ccacfbcedb99a649ae2114
William Carlson <wcarlson81@gmail.com>
2017-08-12 22:25:40 -0500

    final commit

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add (num1, num2)
+  num1 + num2
+end
+
+def subtract (num1, num2)
+  num1 - num2
+end
+
+def sum (numbers)
+  numbers.inject(0, &:+)
+end
+
+def multiply (numbers)
+  numbers.inject(:*)
+end
+
+def power (a, b)
+  a ** b
+end
+
+def factorial num
+  (1..num).inject(:*) || 1
+end
